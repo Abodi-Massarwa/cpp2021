907cef6f590713bb5c0eb1e3f0d441a9ff1d713d
Abedelkareem Massarweh <abodi.msarwi@gmail.com>
2021-03-30 23:22:51 +0300

    commit v-epsilon

---
+++ cmake-build-debug/CMakeFiles/adsBoard.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adsBoard.dir/Board.cpp.obj"
+  "CMakeFiles/adsBoard.dir/Demo.cpp.obj"
+  "adsBoard.exe"
+  "adsBoard.exe.manifest"
+  "adsBoard.pdb"
+  "libadsBoard.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/adsBoard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
